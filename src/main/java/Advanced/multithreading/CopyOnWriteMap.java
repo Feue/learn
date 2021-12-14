83a7c08360571838df18079db66a5f34dc62fd78
Feue <fengjj6@qq.com>
2021-12-14 11:23:47 +0800

    21-12-14 CopyOnWrite

---
+++ src/main/java/Advanced/multithreading/CopyOnWriteMap.java
@@ -0,0 +1,101 @@
+package Advanced.multithreading;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Feue
+ * @create 2021-12-14 10:47
+ */
+public class CopyOnWriteMap<K, V> implements Map<K, V>, Cloneable {
+    private volatile Map<K, V> internalMap;
+
+    public CopyOnWriteMap() {
+        this.internalMap = new HashMap<K, V>();
+    }
+
+    @Nullable
+    @Override
+    public V put(K key, V value) {
+        synchronized (this) {
+            Map<K, V> newMap = new HashMap<>(this.internalMap);
+            V val = newMap.put(key, value);
+            this.internalMap = newMap;
+            return val;
+        }
+    }
+
+    @Override
+    public V get(Object key) {
+        return this.internalMap.get(key);
+    }
+
+    @Override
+    public int size() {
+        return this.internalMap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.internalMap.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return this.internalMap.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return this.internalMap.containsValue(value);
+    }
+
+    @Override
+    public V remove(Object key) {
+        synchronized (this) {
+            Map<K, V> newMap = new HashMap<>(this.internalMap);
+            V val = newMap.remove(key);
+            this.internalMap = newMap;
+            return val;
+        }
+    }
+
+    @Override
+    public void clear() {
+        synchronized (this) {
+            this.internalMap = new HashMap<>();
+        }
+    }
+
+    @NotNull
+    @Override
+    public Set<K> keySet() {
+        return this.internalMap.keySet();
+    }
+
+    @NotNull
+    @Override
+    public Collection<V> values() {
+        return this.internalMap.values();
+    }
+
+    @NotNull
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return this.internalMap.entrySet();
+    }
+
+    @Override
+    public void putAll(@NotNull Map<? extends K, ? extends V> m) {
+        synchronized (this) {
+            Map<K, V> newMap = new HashMap<>(this.internalMap);
+            newMap.putAll(m);
+            this.internalMap = newMap;
+        }
+    }
+}
